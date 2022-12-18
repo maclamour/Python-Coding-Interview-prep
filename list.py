5cd900a70f6f6009c6809648b487179a91ba0d7e
maclamour <maclamour@gmail.com>
2022-12-18 08:46:07 -0500

    remvoe even integers

--- list.py
+++ list.py
@@ -12,3 +12,24 @@ l3 = l[2:3]
 print(l1)
 print(l2)
 print(l3)
+
+list2 =['a', 'b', 'c', 'd', 'e', 'f', 'g']
+print(list2[0:])
+
+print ([1,2]+[3,4])
+
+l5 = [1,2,4,8,10]
+for val in l5:
+    print(val)
+
+def remove_even(lst):
+    odds = []  # Create a new empty list
+    for number in lst:  # Iterate over input list
+        # Check if the item in the list is NOT even
+        # ('%' is the modulus symbol!)
+        if number % 2 != 0:
+            odds.append(number)  # If it isn't even append it to the empty list
+    return odds  # Return the new list
+
+
+print(remove_even([3, 2, 41, 3, 34]))
